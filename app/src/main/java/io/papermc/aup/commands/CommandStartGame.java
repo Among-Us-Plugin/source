e636c978608c076aeae9b44334ed7a2efbc9ef5c
pairnon <janpairnon@gmail.com>
2024-01-19 14:56:18 -0800

    Create some command classes

---
+++ app/src/main/java/io/papermc/aup/commands/CommandStartGame.java
@@ -0,0 +1,16 @@
+package io.papermc.aup.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class CommandStartGame implements CommandExecutor {
+    
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+
+
+        return true;
+    }
+}

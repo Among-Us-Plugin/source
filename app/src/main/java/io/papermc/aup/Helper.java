69fdddb85124c21d95fdd964436cdd87d8e1dd09
pairnon <janpairnon@gmail.com>
2024-03-20 12:48:21 -0700

    Add end message methods

--- app/src/main/java/io/papermc/aup/Helper.java
+++ app/src/main/java/io/papermc/aup/Helper.java
@@ -1,9 +1,12 @@
 package io.papermc.aup;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
 import io.papermc.aup.classes.AmongUsPlayer;
+import io.papermc.aup.classes.Crewmate;
+import io.papermc.aup.classes.Impostor;
 
 @SuppressWarnings("deprecation")
 public class Helper {
@@ -43,6 +46,34 @@ public class Helper {
             }
         }
         return null;
+    }
+
+    // Crewmates win -> send appropriate messages
+    public static void crewmatesWin() {
+        for (AmongUsPlayer a : Helper.amongUsPlayers) {
+            Player p = Helper.getPlayerByDisplayName(a.getPlayerName());
+            if (a instanceof Crewmate) {
+                p.sendTitle(ChatColor.GREEN + "VICTORY", ChatColor.GREEN + "The crewmates won!", 10, 40, 10);
+            }
+            else if (a instanceof Impostor) {
+                p.sendTitle(ChatColor.RED + "DEFEAT", ChatColor.RED + "The crewmates won!", 10, 40, 10);
+            }
+        }
+        return;
+    }
+
+    // Impostors win -> send appropriate messages
+    static void impostorsWin() {
+        for (AmongUsPlayer a : Helper.amongUsPlayers) {
+            Player p = Helper.getPlayerByDisplayName(a.getPlayerName());
+            if (a instanceof Crewmate) {
+                p.sendTitle(ChatColor.GREEN + "DEFEAT", ChatColor.RED + "The impostors won!", 10, 40, 10);
+            }
+            else if (a instanceof Impostor) {
+                p.sendTitle(ChatColor.RED + "VICTORY", ChatColor.GREEN + "The impostors won!", 10, 40, 10);
+            }
+        }
+        return;
     }
 
 }

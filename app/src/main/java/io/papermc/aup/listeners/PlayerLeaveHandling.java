96693461a1b078255351dbfa5215c37a06b2db48
pairnon <janpairnon@gmail.com>
2024-03-20 12:35:55 -0700

    Handle impostors leaving the game

--- app/src/main/java/io/papermc/aup/listeners/PlayerLeaveHandling.java
+++ app/src/main/java/io/papermc/aup/listeners/PlayerLeaveHandling.java
@@ -1,10 +1,15 @@
 package io.papermc.aup.listeners;
 
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 import io.papermc.aup.Helper;
+import io.papermc.aup.classes.AmongUsPlayer;
+import io.papermc.aup.classes.Impostor;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 
 @SuppressWarnings("deprecation")
 public class PlayerLeaveHandling implements Listener {
@@ -13,10 +18,24 @@ public class PlayerLeaveHandling implements Listener {
     @EventHandler
     public void onPlayerQuit(PlayerQuitEvent event) {
         if (Helper.gameRunning == false) {
-                return;
+            return;
         }
+
         String name = event.getPlayer().getDisplayName();
-        System.out.println(name + "left the game!");
+
+        Helper.death(name);
+
+        // If there are no impostors left, end the game
+        // If there is an impostor, do nothing
+        for (AmongUsPlayer a : Helper.amongUsPlayers) {
+            if (a instanceof Impostor && a.isALive()) {
+                return;
+            }
+        }
+
+        Helper.gameRunning = false;
+        Component c = Component.text("Game Ended: There are no more impostors!", NamedTextColor.RED);
+        Bukkit.broadcast(c);
     }
 
 }

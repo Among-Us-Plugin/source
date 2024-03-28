5c12b4b0dd2d9e60d74efc76eb076e4af90e96cf
pairnon <janpairnon@gmail.com>
2024-03-28 10:01:58 -0700

    Add player death handler

---
+++ app/src/main/java/io/papermc/aup/listeners/PlayerDeathHandling.java
@@ -0,0 +1,24 @@
+package io.papermc.aup.listeners;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+
+import io.papermc.aup.Helper;
+
+@SuppressWarnings("deprecation")
+public class PlayerDeathHandling implements Listener {
+
+    // Handle player deaths
+    @EventHandler
+    public void onPlayerDeath(PlayerDeathEvent event) {
+        // If game is not running -> ignore
+        if (Helper.gameRunning == false) {
+            return;
+        }
+
+        String name = event.getPlayer().getDisplayName();
+        Helper.death(name);
+        Helper.checkImpostors();
+    }
+}

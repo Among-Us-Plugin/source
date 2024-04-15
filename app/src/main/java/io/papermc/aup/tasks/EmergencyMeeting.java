50f45163446ba5ceb0cd7691bc1b15a48ba6a037
pairnon <janpairnon@gmail.com>
2024-04-15 12:36:38 -0700

    Fix voting GUI persistence

--- app/src/main/java/io/papermc/aup/tasks/EmergencyMeeting.java
+++ app/src/main/java/io/papermc/aup/tasks/EmergencyMeeting.java
@@ -7,6 +7,7 @@ import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryView;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.SkullMeta;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -75,6 +76,9 @@ public class EmergencyMeeting {
         
         Bukkit.getLogger().info("Voter: " + voterName +  "Voted: " + votedName);
         votes.put(voterName, votedName);
+        
+        InventoryView v = event.getView();
+        v.close();
     }
     
     private static void addPlayersToMeetingBossBars() {

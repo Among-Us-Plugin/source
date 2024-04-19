ceaa73963ceaae4123d368c4e5aa754fcbf0cff4
pairnon <janpairnon@gmail.com>
2024-04-19 14:42:33 -0700

    Allow death messages when game not running

--- app/src/main/java/io/papermc/aup/listeners/DeathMessageHandler.java
+++ app/src/main/java/io/papermc/aup/listeners/DeathMessageHandler.java
@@ -4,11 +4,15 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 
+import io.papermc.aup.Game;
+
 public class DeathMessageHandler implements Listener {
 
     // Prevent publicisation of player deaths
     @EventHandler
     public void onPlayerDeath(PlayerDeathEvent event) {
-        event.deathMessage(null);
+        if (Game.gameRunning) {
+            event.deathMessage(null);
+        }
     }
 }

03475e646607487f8086cc1b674ceeceb73c636a
pairnon <janpairnon@gmail.com>
2024-04-22 08:49:13 -0700

    Allow chat during meetings

--- app/src/main/java/io/papermc/aup/listeners/ChatHandler.java
+++ app/src/main/java/io/papermc/aup/listeners/ChatHandler.java
@@ -1,20 +1,20 @@
 package io.papermc.aup.listeners;
 
-import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
+import io.papermc.aup.Broadcasting;
 import io.papermc.aup.Game;
 
 @SuppressWarnings("deprecation")
 public class ChatHandler implements Listener {
 
-    // Prevent Crewmates from using game chat if a meeting is not active
     @EventHandler
     public void onPlayerChat(AsyncPlayerChatEvent event) {
         if (!Game.gameRunning) { return; }
-        event.setCancelled(true); // This prevents the message from being sent
-        event.getPlayer().sendMessage(ChatColor.RED + "You cannot use chat during the game!");
+        if ( Game.emergencyMeetingInProgress ) { return; }
+        event.setCancelled(true);
+        Broadcasting.sendError(event.getPlayer(), "You cannot use chat during the game!");
     }
 }

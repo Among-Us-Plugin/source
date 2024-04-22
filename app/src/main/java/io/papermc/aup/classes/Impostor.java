9d6b2ca0cdb877f8e05b9c3d24527a35d9a6f13f
pairnon <janpairnon@gmail.com>
2024-04-22 10:44:12 -0700

    Update Impostor class for vent system

--- app/src/main/java/io/papermc/aup/classes/Impostor.java
+++ app/src/main/java/io/papermc/aup/classes/Impostor.java
@@ -3,9 +3,24 @@ package io.papermc.aup.classes;
 import org.bukkit.entity.Player;
 
 public class Impostor extends AmongUsPlayer {
+
+    private boolean isVenting;
     
     public Impostor(Player player) {
         super(player);
+        isVenting = false;
+    }
+
+    public boolean isVenting() {
+        return isVenting;
+    }
+
+    public void startVenting() {
+        isVenting = true;
+    }
+
+    public void stopVenting() {
+        isVenting = false;
     }
 
 }

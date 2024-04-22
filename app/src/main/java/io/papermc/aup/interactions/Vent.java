d46779a71606b01da8cbf82251cda53a9bb52df8
pairnon <janpairnon@gmail.com>
2024-04-22 10:53:50 -0700

    Add ghost mode for venting impostors

--- app/src/main/java/io/papermc/aup/interactions/Vent.java
+++ app/src/main/java/io/papermc/aup/interactions/Vent.java
@@ -1,11 +1,20 @@
 package io.papermc.aup.interactions;
 
+import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
+
+import io.papermc.aup.classes.AmongUsPlayer;
+import io.papermc.aup.classes.Impostor;
 
 public class Vent {
     
     public static void run(Player player) {
         player.sendMessage("you vented");
+        AmongUsPlayer a = AmongUsPlayer.getAmongUsPlayerByDisplayName(player.getDisplayName());
+        if ( !(a instanceof Impostor) ) { return; }
+        Impostor impostor = (Impostor)a;
+        player.setGameMode(GameMode.SPECTATOR);
+        impostor.startVenting();
     }
 
 }

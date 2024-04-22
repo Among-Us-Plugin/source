397b0e379acc66be95f688ddc83ec264f7a2eb10
pairnon <janpairnon@gmail.com>
2024-04-22 10:54:20 -0700

    Prevent impostor movement while venting

--- app/src/main/java/io/papermc/aup/listeners/MovementHandler.java
+++ app/src/main/java/io/papermc/aup/listeners/MovementHandler.java
@@ -1,10 +1,13 @@
 package io.papermc.aup.listeners;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
 
 import io.papermc.aup.Game;
+import io.papermc.aup.classes.AmongUsPlayer;
+import io.papermc.aup.classes.Impostor;
 
 public class MovementHandler implements Listener {
     
@@ -16,6 +19,12 @@ public class MovementHandler implements Listener {
             if (event.getFrom().getX() == event.getTo().getX() && event.getFrom().getZ() == event.getTo().getZ()) { return; }
             event.setCancelled(true);
         }
-        
+
+        Player p = event.getPlayer();
+        AmongUsPlayer a = AmongUsPlayer.getAmongUsPlayerByDisplayName(p.getDisplayName());
+        if ( !(a instanceof Impostor) ) { return; }
+        Impostor impostor = (Impostor)a;
+        if ( !impostor.isVenting()) { return; }
+        event.setCancelled(true);
     }
 }

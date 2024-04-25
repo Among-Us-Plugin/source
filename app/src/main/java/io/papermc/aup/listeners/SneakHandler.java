e6cc66d042a324b3b0142eef285c1de2b8f97f24
pairnon <janpairnon@gmail.com>
2024-04-25 10:56:33 -0700

    Only allow vent exiting during game

--- app/src/main/java/io/papermc/aup/listeners/SneakHandler.java
+++ app/src/main/java/io/papermc/aup/listeners/SneakHandler.java
@@ -4,12 +4,14 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerToggleSneakEvent;
 
+import io.papermc.aup.Game;
 import io.papermc.aup.interactions.Vent;
 
 public class SneakHandler implements Listener {
     
     @EventHandler
     public void onPlayerToggleSneak(PlayerToggleSneakEvent event) {
+        if (!Game.gameRunning) { return; }
         if (event.isSneaking()) {
             Vent.handleSneak();
         }

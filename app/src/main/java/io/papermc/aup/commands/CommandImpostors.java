071526ce28ef4165129d0db644e41faa83bbe92e
pairnon <janpairnon@gmail.com>
2024-05-02 11:30:36 -0700

    Refactor

--- app/src/main/java/io/papermc/aup/commands/CommandImpostors.java
+++ app/src/main/java/io/papermc/aup/commands/CommandImpostors.java
@@ -7,7 +7,7 @@ import org.bukkit.entity.Player;
 
 import io.papermc.aup.Broadcasting;
 import io.papermc.aup.Game;
-import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 
 public class CommandImpostors implements CommandExecutor {
 
@@ -17,14 +17,12 @@ public class CommandImpostors implements CommandExecutor {
         Player player = (Player) sender;
 
         if (args.length == 0) {
-            Component c = Component.text("Number of impostors per game: " + Game.numImpostors);
-            player.sendMessage(c);
+            Broadcasting.sendSignedMessageToPlayer(player, "Number of impostors per game: " + Game.numImpostors, NamedTextColor.AQUA);
             return true;
         } else if (args.length == 1) {
             if (Integer.parseInt(args[0]) >= 1 && Integer.parseInt(args[0]) <= 5) {
                 Game.numImpostors = Integer.parseInt(args[0]);
-                Component c = Component.text("Set number of impostors to: " + Game.numImpostors);
-                player.sendMessage(c);
+                Broadcasting.sendSignedMessageToPlayer(player, "Set number of impostors to: " + Game.numImpostors, NamedTextColor.AQUA);
                 return true;
             }
             else {

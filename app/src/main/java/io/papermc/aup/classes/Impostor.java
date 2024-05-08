8e295b199888a68f959339d8c0c0abbc11a9a851
pairnon <janpairnon@gmail.com>
2024-05-08 12:16:07 -0700

    Add cooldowns to Impostor class

--- app/src/main/java/io/papermc/aup/classes/Impostor.java
+++ app/src/main/java/io/papermc/aup/classes/Impostor.java
@@ -5,10 +5,30 @@ import org.bukkit.entity.Player;
 public class Impostor extends AmongUsPlayer {
 
     private boolean isVenting;
+    private int ventCooldown;
+    private int killCooldown;
     
     public Impostor(Player player) {
         super(player);
         isVenting = false;
+        ventCooldown = 0;
+        killCooldown = 0;
+    }
+
+    public void setVentCooldown(int v) {
+        ventCooldown = v;
+    }
+
+    public void setKillCooldown(int k) {
+        killCooldown = k;
+    }
+    
+    public int getVentCooldown() {
+        return ventCooldown;
+    }
+
+    public int getKillCooldown() {
+        return killCooldown;
     }
 
     public boolean isVenting() {

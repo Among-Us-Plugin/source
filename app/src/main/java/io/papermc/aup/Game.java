0f343d6c36b1d9617f4fec4e11004f8c383c34ec
pairnon <janpairnon@gmail.com>
2024-05-16 10:09:25 -0700

    Increase discussion period length

--- app/src/main/java/io/papermc/aup/Game.java
+++ app/src/main/java/io/papermc/aup/Game.java
@@ -37,8 +37,8 @@ public class Game {
     public static boolean gameRunning = false;
     public static boolean emergencyMeetingInProgress = false;
 
-    public static int discussionPeriodDurationInSeconds = 2;
     public static int meetingDurationInSeconds = 10;
+    public static int discussionPeriodDurationInSeconds = meetingDurationInSeconds / 2;
     public static int meetingCooldownInSeconds = meetingDurationInSeconds + 10;
     public static int ventCooldownInSeconds = 10;
     public static int killCooldownInSeconds = 10;
